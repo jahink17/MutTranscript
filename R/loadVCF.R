51b901482db71bcc64fe8ed95fd35cd316d5e1ea
jahink17 <jahinkabir17@gmail.com>
2024-11-04 18:00:26 -0500

    Added documentation for loadVcf().

--- R/loadVCF.R
+++ R/loadVCF.R
@@ -1,17 +1,24 @@
 # To-do: Need to check if this works on test file
 # To-do: Need to add functionality to create index file if not present
 # To-do: Need to extend to hg38
-#' Title
+#' Load a VCF file into R
 #'
-#' @param pathToVcf
-#' @param genome
+#' This function loads a VCF (Variant Call Format) file, specified by the file path, into R using the `VariantAnnotation` package.
+#' It returns a VCF object, which can be further used for variant analysis.
 #'
-#' @return
+#' @param pathToVcf A string representing the file path to the VCF file.
+#' @param genome A string specifying the reference genome version (e.g. "hg19")
+#'
+#' @return A `VCF` object containing the variants from the VCF file.
+#' @import VariantAnnotation
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' vcfData <- loadVCF("/path/to/file.vcf", "hg19")
+#' }
 loadVcf <- function(pathToVcf, genome) {
-  vcffile <- VariantAnnotation::VcfFile(pathToVcf, index = paste(pathToVcf, "tbi", sep="."))
-  vcf <- VariantAnnotation::readVcf(vcffile, genome = "hg19")
+  vcfFile <- VariantAnnotation::VcfFile(pathToVcf, index = paste(pathToVcf, "tbi", sep="."))
+  vcf <- VariantAnnotation::readVcf(vcfFile, genome = "hg19")
   return(vcf)
 }
